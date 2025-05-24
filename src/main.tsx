62393cd7c572cf93b51dd72bf9925eff61b15046
Nikita2993-Z <zinchenko.nikita93@gmail.com>
2025-05-24 21:17:50 +0300

    123

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Toaster } from 'react-hot-toast';
 import { createRoot } from 'react-dom/client'
 import 'modern-normalize/modern-normalize.css';
 import './index.css'
-import App from './App'
+import App from './components/App/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
